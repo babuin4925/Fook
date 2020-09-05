374f67f5145aa37bfbb35f4ee2d1017cd33f3c01
Imache <babuin4925@gmail.com>
2020-09-05 11:35:02 +0300

    FookIsObj
    this save is before I made fook into a button

---
+++ Assets/Scripts/ScoreManagement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManagement : MonoBehaviour
+{
+    public int clickPow;
+    public int score;
+    public Text scoreTxt;
+    private void Update()
+    {
+        scoreTxt.text = score.ToString();
+    }
+    public void ButtonClick()
+    {
+        score = score + clickPow; 
+    }
+
+}
