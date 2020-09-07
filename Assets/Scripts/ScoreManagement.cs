f59148dcd9ebbb3215357b8fee7d24c71d21edd2
Imache <babuin4925@gmail.com>
2020-09-07 21:19:34 +0300

    FookNormal
    Fook is now an object not a button

--- Assets/Scripts/ScoreManagement.cs
+++ Assets/Scripts/ScoreManagement.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,13 +8,29 @@ public class ScoreManagement : MonoBehaviour
     public int clickPow;
     public int score;
     public Text scoreTxt;
+
+    public LayerMask LayerMask;
+    private Vector2 camPos;
+
+    private void Start()
+    {
+        Vector2 camPos = new Vector2(transform.position.x, transform.position.y);
+}
     private void Update()
     {
-        scoreTxt.text = score.ToString();
+        Vector2 direct = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        RaycastHit2D hit = Physics2D.Raycast(camPos, direct);
+        if (hit)
+        {
+            Debug.Log("The ray hit " + hit.collider.name);
+            scoreViewer();
+        }
+
     }
-    public void ButtonClick()
+    public void scoreViewer()
     {
-        score = score + clickPow; 
+        score = score + clickPow;
+        scoreTxt.text = score.ToString();
     }
 
 }
