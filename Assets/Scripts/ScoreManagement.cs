d0cb5a9b45143dc56aa6f492547dc501f0a5ce1e
Imache <babuin4925@gmail.com>
2020-09-11 19:11:52 +0300

    ClickWorking!
    Fook is clickable and score goes up!

--- Assets/Scripts/ScoreManagement.cs
+++ Assets/Scripts/ScoreManagement.cs
@@ -8,24 +8,8 @@ public class ScoreManagement : MonoBehaviour
     public int clickPow = 1;
     public int score;
     public Text scoreTxt;
-    private GameObject fook;
-    public Sprite idle;
-    private void Start()
-    {
-        fook = GameObject.Find("Fook");
-    }
-    private void Update()
-    {
-        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit2D hitInfo = Physics2D.GetRayIntersection(mouseRay);
-        if (hitInfo.collider!= null)
-        {
-            Debug.Log("The ray hit " + hitInfo.collider.name);
-            fook.GetComponent<SpriteRenderer>().sprite = idle;
-        }
 
-    }
-    public void scoreViewer()
+    public void scoreIncrease()
     {
         score = score + clickPow;
         scoreTxt.text = score.ToString();
---
+++ Assets/Scripts/StatesAnimationsAndBasicControlls.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatesAnimationsAndBasicControlls : MonoBehaviour
+{
+    bool isSitting = true;
+    bool isSittingIdle = true;
+    bool isPlaying = false;
+    bool isClicked = false;
+
+    public Sprite idle;
+    public Sprite sitting;
+    public Sprite idle_clicked;
+
+    public ScoreManagement Score;
+
+    void Update()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hitInfo = Physics2D.GetRayIntersection(mouseRay);
+        if (hitInfo.collider != null)
+        {
+            Debug.Log("The ray hit " + hitInfo.collider.name);
+            Stand();
+
+            if (Input.GetMouseButton(0))
+            {
+                SpriteChange(idle_clicked);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                Click();
+            }
+        }
+        else
+        {
+            Sit();
+        }
+        
+    }
+
+    public void Sit()
+    {
+        isSitting = true;
+        SpriteChange(sitting);
+    }
+    public void Stand()
+    {
+        isSitting = false;
+        SpriteChange(idle);
+    }
+    public void EarShake()
+    {
+        isSittingIdle = false;
+        //starts animation
+    }
+    public void Play()
+    {
+        isPlaying = true;
+        //Starts playing animation
+    }
+    public void Click()
+    {
+        Score.scoreIncrease();
+    }
+
+
+    public void SpriteChange(Sprite neededSprite)
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = neededSprite;
+    }
+}
