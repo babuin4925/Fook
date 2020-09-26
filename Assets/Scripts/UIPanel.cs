024045a5f9d1f98e8bbb540e67e782aa2ce41451
Imache <babuin4925@gmail.com>
2020-09-26 18:21:16 +0300

    PanelMovement

---
+++ Assets/Scripts/UIPanel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanel : MonoBehaviour
+{
+    public RectTransform panelTransform;
+
+    private bool isSlidingLeft = false, isSlidingRight = false, onScreen= false;
+
+    public float defaultOffScreenPos = 10.75f, defaultOnScreenPos = 7f;
+    //offscreen: x =7  onscreen x= 10.75
+    void Update()
+    {
+        if (isSlidingLeft)
+        {
+            if (panelTransform.position.x > defaultOnScreenPos)
+            {
+                panelTransform.position += transform.up * Time.deltaTime * 5;
+            }
+            else
+            {
+                isSlidingLeft = false;
+                onScreen = true;
+            }
+        }
+        Debug.Log(Input.mousePosition.x);
+        if (onScreen && Input.mousePosition.x < 600f)
+        {
+            isSlidingRight = true;
+            if (panelTransform.position.x < defaultOffScreenPos&& isSlidingRight)
+            {
+                panelTransform.position += -transform.up * Time.deltaTime * 5;
+            }
+            else
+            {
+                onScreen = false;
+                isSlidingRight = false;
+            }
+        }
+    }
+    public void Slide()
+    {
+        if (!onScreen&&!isSlidingLeft)
+        {
+            isSlidingLeft = true;
+        }
+    }
+}
